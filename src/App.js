7066300aeeb045faa4c71040118ba648dd36b122
Joe <joseph.lee3454@gmail.com>
2021-01-07 13:37:27 -0800

    made some props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card lastname={"lee"} firstname={"joe"} age={28} hair={"dirty blonde"}/>
+      <Card lastname={"doe"} firstname={"jon"} age={58} hair={"black"}/>
+      <Card lastname={"ragnar"} firstname={"fin"} age={20} hair={"blonde"}/>
+      <Card lastname={"patenoude"} firstname={"patty"} age={78} hair={"grey"}/>
     </div>
   );
 }
 
 export default App;
+ 
